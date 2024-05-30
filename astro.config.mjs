fb792b3109b256733538cb1822189e26c46b0206
obahamonde <oscarmartinbahamondemunoz@gmail.com>
2024-05-30 17:34:27 +0000

    landing

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "astro/config";
+import UnoCSS from "unocss/astro";
+import vue from "@astrojs/vue";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [UnoCSS(), vue()],
+});
